8678775f54d85fd93648124630ff650fe6b80d80
Miguel Silva <a74601@alunos.uminho.pt>
2018-04-30 21:26:58 +0100

    Add event logging to file (#2)

--- falcon-tracer/falcon/core/events/handling/logger/event_logger.py
+++ falcon-tracer/falcon/core/events/handling/logger/event_logger.py
@@ -1,8 +1,12 @@
+import logging
 import ujson as json
 from event_parser import EventParser
 from falcon.core.events.handling.base_handler import BaseHandler
 
 class EventLogger(BaseHandler):
+    def __init__(self):
+        self.logger = logging.getLogger('event')
+
     def handle(self, cpu, data, size):
         data = EventParser.parse(cpu, data, size)
         if data is None:
@@ -12,4 +16,4 @@ class EventLogger(BaseHandler):
             data = [data]
 
         for event in data:
-            print json.dumps(event)
+            self.logger.info(json.dumps(event))
