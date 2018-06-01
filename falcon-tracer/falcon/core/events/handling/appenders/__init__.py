9e3f7292214ea4b7414b3f44973fcd5b58a7d278
Francisco Neves <fntneves@gmail.com>
2018-06-01 19:05:23 +0100

    Add kafka writer

--- falcon-tracer/falcon/core/events/handling/appenders/__init__.py
+++ falcon-tracer/falcon/core/events/handling/appenders/__init__.py
@@ -1,2 +1,3 @@
 from falcon.core.events.handling.appenders.json_writer import JsonWriter
 from falcon.core.events.handling.appenders.binary_writer import BinaryWriter
+from falcon.core.events.handling.appenders.kafka_writer import KafkaWriter
