46b8ad9c7fd6760693b6a5d0a6dce7b6a3dcbb59
Francisco Neves <fntneves@gmail.com>
2018-07-26 11:41:09 +0100

    Add more debug messages

--- falcon-tracer/falcon/core/events/handling/writers/kafka_writer.py
+++ falcon-tracer/falcon/core/events/handling/writers/kafka_writer.py
@@ -25,6 +25,10 @@ class KafkaWriter:
         # been successfully delivered or failed permanently.
         topic = self.topic_for_event(event)
 
+        if logging.getLogger().getEffectiveLevel() == logging.DEBUG:
+            logging.debug('Delivering event type [{}] to {} [{}]'.format(
+                event._type, topic['name'], topic['partition']))
+
         self._producer.produce(topic['name'], buffer(event.to_bytes()), partition=topic['partition'], callback=KafkaWriter.delivery_report)
 
     def close(self):
