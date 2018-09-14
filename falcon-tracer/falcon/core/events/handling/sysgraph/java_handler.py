1024e23f89b399a0a639f7d670056f1757e697a8
Francisco Neves <fntneves@gmail.com>
2018-09-14 15:59:03 +0100

    Fix

--- falcon-tracer/falcon/core/events/handling/sysgraph/java_handler.py
+++ falcon-tracer/falcon/core/events/handling/sysgraph/java_handler.py
@@ -5,12 +5,12 @@ import struct
 class JavaProcessHandler():
     def __init__(self, host, port):
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._sock.connect((host, int(port))
-
-    def __del__(self):
-        if (self._sock is not None):
-            self._sock.close()
+        self._sock.connect((host, int(port)))
 
     def handle(self, cpu, event):
         pid_binary = struct.pack('!i', event.pid)
         self._sock.send(pid_binary)
+
+    def __del__(self):
+        if (self._sock is not None):
+            self._sock.close()
