3582f4a7ed4952d596a1adbfb3c52eafa8ba8cb9
Francisco Neves <fntneves@gmail.com>
2019-12-17 13:09:34 +0000

    remove missing file

--- falcon-tracer/falcon/core/events/handling/__init__.py
+++ falcon-tracer/falcon/core/events/handling/__init__.py
@@ -1,4 +1,3 @@
 import writers
 from falcon.core.events.handling.event_logger import FalconEventLogger
-from falcon.core.events.handling.event_dispatcher import EventDispatcher
 from falcon.core.events.handling.base_handler import BaseHandler
